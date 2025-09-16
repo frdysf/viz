6ff1cbf616fd0cb63dab27171eebd9ee79a78478
Farida Yusuf <f.yusuf@qmul.ac.uk>
2025-09-16 13:37:42 +0000

    Delete files

--- tests/test_viz.py
+++
@@ -1,4 +0,0 @@
-# TODO: test for all transforms with dummy data (generated from prob distributions)
-# TODO: choose n_components from 2 or 3 and test for both
-# create and save generated figures in ./img
-# don't forget to use pytest.mark.parameterize
